41ec5b7622a2606775bc0196815baca1a39d824d
Max Luebbering <m.luebbering@gmail.com>
2021-01-21 10:57:36 +0100

    Implemented uniform noise toy dataset

---
+++ src/outlier_hub/datasets/toy_datasets/uniform_noise/factory.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from data_stack.dataset.factory import BaseDatasetFactory
+from data_stack.dataset.iterator import DatasetIteratorIF
+from data_stack.dataset.meta import MetaFactory
+from outlier_hub.datasets.toy_datasets.uniform_noise.iterator import UniformNoiseIterator
+from typing import List, Tuple, Dict, Any
+
+
+class UniformNoiseFactory(BaseDatasetFactory):
+    """Builds a half moon dataset.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def _get_iterator(self, num_samples: int, classes: List[int], hypercube: List[Tuple[int, int]], seed: int = 1):
+        meta = MetaFactory.get_iterator_meta(sample_pos=0, target_pos=1, tag_pos=2)
+        return UniformNoiseIterator(seed=seed, num_samples=num_samples, classes=classes, hypercube=hypercube), meta
+
+    def get_dataset_iterator(self, config: Dict[str, Any] = None) -> DatasetIteratorIF:
+        return self._get_iterator(**config)
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import torch
+
+    classes = [0, 1]
+    hypercube = [(-1, 1), (3, 6)]
+    config = {"seed": 1, "classes": classes, "num_samples": [2000, 2000], "hypercube": hypercube}
+    factory = UniformNoiseFactory()
+    iterator, meta = factory.get_dataset_iterator(config)
+
+    samples, targets = zip(*[(s, t) for s, t, _ in iterator])
+
+    sample_tensor = torch.stack(samples)
+    class_0_samples = sample_tensor[torch.IntTensor(targets) == 0]
+    class_1_samples = sample_tensor[torch.IntTensor(targets) == 1]
+
+    plt.scatter(*list(zip(*class_0_samples)), color='red', s=1)
+    plt.scatter(*list(zip(*class_1_samples)), color='blue', s=1)
+    plt.show()
---
+++ src/outlier_hub/datasets/toy_datasets/uniform_noise/iterator.py
@@ -0,0 +1,35 @@
+import torch
+from data_stack.dataset.iterator import DatasetIterator
+from typing import List, Tuple
+import numpy as np
+
+
+class UniformNoiseIterator(DatasetIterator):
+
+    def __init__(self, num_samples: List[int], classes: List[int], hypercube: List[Tuple[int, int]], seed: int = 1):
+        """
+        Creates a dataset iterator for uniform noise
+        Args:
+         ...
+        """
+        rng = np.random.default_rng(seed=seed)
+        min_vals = list(zip(*hypercube))[0]
+        max_vals = list(zip(*hypercube))[1]
+        X_list = []
+        y_list = []
+        for i in range(len(classes)):
+            X_list.append(rng.uniform(low=min_vals, high=max_vals, size=[num_samples[i], len(hypercube)]))
+            y_list.append([classes[i]]*num_samples[i])
+
+        self.X = torch.Tensor(np.concatenate(X_list))
+        self.y = torch.Tensor(np.concatenate(y_list))
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, index: int):
+        """ Returns the sample and target of the dataset at given index position.
+        :param index: index within dataset
+        :return: sample, target, tag
+        """
+        return self.X[index], self.y[index], self.y[index]
