a779cba6bb2750b05a93c12d1e4965eb19259239
Max Luebbering <m.luebbering@gmail.com>
2021-01-22 23:26:18 +0100

    Implemented translation option for circles dataset

--- src/outlier_hub/datasets/toy_datasets/circles/factory.py
+++ src/outlier_hub/datasets/toy_datasets/circles/factory.py
@@ -14,9 +14,10 @@ class CirclesFactory(BaseDatasetFactory):
     def __init__(self):
         super().__init__()
 
-    def _get_iterator(self, split: str, scale_factor: float, noise_std: float, num_samples: List[int], seed: int = 1):
+    def _get_iterator(self, split: str, scale_factor: float, noise_std: float, num_samples: List[int], seed: int = 1,
+                      translation: List[int] = None):
         meta = MetaFactory.get_iterator_meta(sample_pos=0, target_pos=1, tag_pos=2)
-        return CirclesIterator(seed, noise_std, num_samples, scale_factor), meta
+        return CirclesIterator(seed, noise_std, num_samples, scale_factor, translation), meta
 
     def get_dataset_iterator(self, config: Dict[str, Any] = None) -> DatasetIteratorIF:
         return self._get_iterator(**config)
@@ -26,7 +27,7 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     import torch
 
-    config = {"seed": 1, "scale_factor": 0.5, "noise_std": 0.1, "num_samples": [500, 500]}
+    config = {"split": "train", "seed": 1, "scale_factor": 0.5, "noise_std": 0.1, "num_samples": [500, 500], "translation": [3, 5]}
 
     factory = CirclesFactory()
     iterator, meta = factory.get_dataset_iterator(config)
--- src/outlier_hub/datasets/toy_datasets/circles/iterator.py
+++ src/outlier_hub/datasets/toy_datasets/circles/iterator.py
@@ -6,7 +6,7 @@ from sklearn.datasets import make_circles
 
 class CirclesIterator(DatasetIterator):
 
-    def __init__(self, seed: int, noise_std: float, num_samples: List[int], scale_factor: float):
+    def __init__(self, seed: int, noise_std: float, num_samples: List[int], scale_factor: float, translation: List[int] = None):
         """
         Creates a dataset iterator for the circle within circle dataset
 
@@ -16,8 +16,11 @@ class CirclesIterator(DatasetIterator):
             num_samples (List): List of length 2 indicating the number of samples for each half moon
             scale_factor (float): Scale factor between inner and outer circle in the range (0, 1)
         """
+
         self.X, self.y = make_circles(n_samples=num_samples, shuffle=True, noise=noise_std, random_state=seed, factor=scale_factor)
         self.X = torch.Tensor(self.X)
+        if translation is not None:
+            self.X = self.X + torch.Tensor(translation)
         self.y = torch.Tensor(self.y)
 
     def __len__(self):
