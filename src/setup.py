887f32f04f27b635671969b0205fec431a9232dd
Max Lübbering <le1nux@users.noreply.github.com>
2021-01-23 13:51:52 +0100

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ with open("../README.md", "r") as fh:
 
 setup(
     name='outlier_hub',
-    version='0.0.2',
+    version='0.0.3',
     author='Max Luebbering, Rajkumar Ramamurthy, Michael Gebauer',
     description="Outlierhub, a collection of machine learning datasets.",
     long_description=long_description,
