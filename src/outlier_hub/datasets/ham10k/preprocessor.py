8331468cfa4382be78fa7d0898354234ee5a2e9f
Ilhamcengiz Henk <cengiz.henk@outlook.com>
2022-01-26 16:27:48 +0100

    Squashed commit of the following:
    commit f908aef83acc358816be6a3343d8fa1d7ed8c14a
    Merge: 1fb0f80 40d1011
    Author: Ilhamcengiz Henk <cengiz.henk@outlook.com>
    Date:   Wed Jan 26 10:49:37 2022 +0100
    
        refactoring
    
    commit 1fb0f80dd0b7083ae4a12ab3aff29d74f48c5aae
    Author: Ilhamcengiz Henk <cengiz.henk@outlook.com>
    Date:   Mon Dec 6 08:34:34 2021 +0100
    
        Rebase: squash all commits depending ham10k redesign
    
        renamed old ham10k dataset to start from scratch
    
        added files from earlier development factory is advanced, now we have to make retrieving data work properly
    
        deleted old ham10k implementation, because it interferes with new code
    
        preprocess.py is nearly done
    
        working redesign of ham10k and it downloads the data from the web, less manual prepare is needed
    
        refactoring
    
        modified path data, so it does not need to download every time we start the script
    
        updated gitignore, cause of dewnloaded data in ham10k
    
    commit 40d1011bf60831cc97d7dd7d1244dc08abc73e7d
    Author: Ilhamcengiz Henk <cengiz.henk@outlook.com>
    Date:   Tue Dec 7 12:45:31 2021 +0100
    
        updated gitignore, cause of dewnloaded data in ham10k
    
    commit e3da7ad26c998e8bb7431317b13eb5461c35bb21
    Author: Ilhamcengiz Henk <cengiz.henk@outlook.com>
    Date:   Tue Dec 7 12:44:16 2021 +0100
    
        modified path data, so it does not need to download every time we start the script
    
    commit 6521bc61201168b5d8c5052bef03b611afe65bb9
    Author: Ilhamcengiz Henk <cengiz.henk@outlook.com>
    Date:   Mon Dec 6 18:21:31 2021 +0100
    
        refactoring
    
    commit a008fa25b5d5f1b190114ff3a4503f35c573863c
    Author: Ilhamcengiz Henk <cengiz.henk@outlook.com>
    Date:   Mon Dec 6 14:56:58 2021 +0100
    
        working redesign of ham10k and it downloads the data from the web, less manual prepare is needed
    
    commit 208bdb125feaa96379cb5fff7e7872b4183bd36f
    Author: Ilhamcengiz Henk <cengiz.henk@outlook.com>
    Date:   Mon Dec 6 13:01:39 2021 +0100
    
        preprocess.py is nearly done
    
    commit 5d86ba8db9dcd906a4a245fb44ff8bb9d35251a0
    Author: Ilhamcengiz Henk <cengiz.henk@outlook.com>
    Date:   Mon Dec 6 09:13:12 2021 +0100
    
        deleted old ham10k implementation, because it interferes with new code
    
    commit d990af93f2bbf68bd58f8d0796e5e98f0d865a5e
    Author: Ilhamcengiz Henk <cengiz.henk@outlook.com>
    Date:   Mon Dec 6 08:37:01 2021 +0100
    
        added files from earlier development factory is advanced, now we have to make retrieving data work properly
    
    commit 7b32010077c52240bc5c21122a0335934f0ea204
    Author: Ilhamcengiz Henk <cengiz.henk@outlook.com>
    Date:   Mon Dec 6 08:34:34 2021 +0100
    
        renamed old ham10k dataset to start from scratch
    
    commit 37b5bf9b5a09658c3c59166327d80da8cece2983
    Author: Ilhamcengiz Henk <cengiz.henk@outlook.com>
    Date:   Fri Nov 12 18:22:38 2021 +0100
    
        redesigned - h5py does not consume to much space -saving images as bytes instead of numpys - https://blade6570.github.io/soumyatripathy/hdf5_blog.html

--- src/outlier_hub/datasets/ham10k/preprocessor.py
+++ src/outlier_hub/datasets/ham10k/preprocessor.py
@@ -1,120 +1,106 @@
-import csv
+
 import os
 import glob
 import tempfile
-import h5py
 import matplotlib.image as mpimg
+from typing import Optional, Tuple, List
 import numpy as np
-
-from data_stack.util.logger import logger
+from zipfile import ZipFile
+from PIL import Image
+from torchvision import transforms
+import h5py
+import pandas as pd
 from data_stack.io.resources import StreamedResource, ResourceFactory
 from data_stack.io.storage_connectors import StorageConnector
-from typing import Tuple, List
-
-
-def _get_raw_split_samples(samples_identifier: str) -> List[str]:
-    """
-    function to load folder content into arrays and then it returns that same array
-    @param samples_identifier: path to samples, here i.e. images
-    @return: sorted list of paths of raw samples
-    """
-    # Put filespaths  into lists and return them:
-    raw_samples_paths = []
-    for file in sorted(glob.glob(samples_identifier + '/*.jpg')):
-        raw_samples_paths.append(file)
-    logger.debug(f"Length Check - raw sample paths - 10015: {len(raw_samples_paths) == 10015}")
-
-    return raw_samples_paths
-
-
-def _get_raw_split_targets(target_identifier: str) -> List[np.ndarray]:
-    """
-    function to load folder content into arrays and then it returns that same array
-    @param targets_identifier: path to metadata.csv file
-    @return: sorted list with ISIC ID of metadata in tupels, each sample gets a Tupel with 8 entries
-    """
-    # Put rows as Tuples into lists and return them:
-    with open(target_identifier, newline='') as targets:
-        reader = csv.reader(targets)
-        targets_list = [np.array(row) for row in reader]
-        # sort by ISIC ID -> target[1]
-        targets_list = sorted(targets_list, key=lambda target: target[1])
-        # delete last element, because it is the not needed header of csv
-        targets_list = targets_list[:10015]
-        logger.debug(f"Length Check - raw meta data - 10015: {len(targets_list) == 10015}")
-        logger.debug(f"Length Check - single tuples - 8: {len(targets_list[0]) == 8}")
-        logger.debug(f"test print of a entry of target_list: \n {targets_list[0]}")
-
-    return targets_list
-
-
-def _preprocess_split(h5py_file: h5py.File,
-                      split_name: str,
-                      samples_identifier: str,
-                      target_identifier: str):
-    logger.debug(f"calling _preprocess_split(h5py_file, split_name, samples_identifier, target_identifier)")
-
-    # split_samples, split_targets = _get_raw_dataset_split(samples_identifier, target_identifier)
-    logger.debug(f" _get_raw_split_samples(samples_identifier: str) -> List[str]")
-    split_samples = _get_raw_split_samples(samples_identifier)
-
-    logger.debug(f" calling _get_raw_split_targets(target_identifier: str) -> List[np.ndarray]")
-    split_targets = _get_raw_split_targets(target_identifier)
-
-    sample_location = os.path.join(split_name, "samples")
-    target_location = os.path.join(split_name, "targets")
-
-    logger.debug(f" prepare and calling sample_dset = h5py_file.create_dataset")
-    # samples are images here, which can be intepreted as numpy ndarrays:
-    # so a colored image has height*width pixels, each pixel contains three values representing RGB-Color
-    height = 450
-    width = 600
-    rgb_channel = 3
-    sample_dset = h5py_file.create_dataset(sample_location,
-                                           shape=(len(split_samples), height, width, rgb_channel),
-                                           dtype=int)
-
-    logger.debug(f" prepare and calling target_dset = h5py_file.create_dataset")
-    # h5py cannot save np.ndarrays with strings by default, costum dtype must be created
-    utf8_type = h5py.string_dtype('utf-8')
-
-    # There are 8 meta information for each sample
-    metadata_info_amount = 8
-
-    target_dset = h5py_file.create_dataset(target_location,
-                                           shape=(len(split_targets), metadata_info_amount,),
-                                           dtype=utf8_type)
-
-    logger.debug(f"enrich h5py_file with sample data")
-    for cnt, sample in enumerate(split_samples):
-        sample_dset[cnt:cnt + 1, :, :] = mpimg.imread(sample)
-
-    logger.debug(f"enrich h5py_file with sample data")
-    for cnt, target in enumerate(split_targets):
-        target_dset[cnt] = target
-
+from pathlib import Path
 
 class HAMPreprocessor:
 
     def __init__(self, storage_connector: StorageConnector):
         self.storage_connector = storage_connector
-        # TODO right now only one split is needed
-        self.split_names = ["raw"]
+
+    def unzip_samples(self, sample_file: str):
+
+        new_filepath = sample_file.split('.')[0]
+
+        zip_file = ZipFile(sample_file, 'r')
+
+        zip_infos = zip_file.infolist()
+
+        for zipinfo in zip_infos:
+            archive_filename = zipinfo.filename
+
+            zipinfo.filename = os.path.basename( zipinfo.filename)
+
+            if not zipinfo.filename == '' :
+                zip_file.extract(member = archive_filename, path = new_filepath)
+
+        zip_file.close()
+
+        os.remove(sample_file)
+
+        return new_filepath
 
     def preprocess(self,
-                   dataset_identifier: str,
-                   samples_identifier: str,
-                   targets_identifier: str) -> StreamedResource:
+                   raw_samples_identifier: str,
+                   raw_targets_identifier: str,
+                   prep_dataset_identifier: str):
+
+        self.split_names = ["raw"]
+
+        # following line is needed, if data is not already retrieved
+        raw_samples_dir = self.unzip_samples(raw_samples_identifier)
+        raw_targets_dir = self.unzip_samples(raw_targets_identifier)
+
+        # following line is needed, when data is already provided
+        #raw_samples_identifier = raw_samples_identifier.split('.')[0]
+        #raw_samples_identifier = raw_samples_identifier.split('.')[0]
+
+        if not os.path.exists(prep_dataset_identifier):
+            os.makedirs(os.path.dirname(prep_dataset_identifier), exist_ok=True)
+
+
         with tempfile.TemporaryFile() as temp_file:
             with h5py.File(temp_file, 'w') as h5py_file:
-                for split_name in self.split_names:
-                    _preprocess_split(h5py_file,
-                                      split_name,
-                                      samples_identifier,
-                                      targets_identifier)
+                self.prepare_dataset(h5py_file,
+                                    raw_samples_dir,
+                                    raw_targets_dir)
                 h5py_file.flush()
                 temp_file.flush()
-            streamed_resource = ResourceFactory.get_resource(dataset_identifier, temp_file)
-            self.storage_connector.set_resource(dataset_identifier, streamed_resource)
-            streamed_resource = self.storage_connector.get_resource(dataset_identifier)
+            streamed_resource = ResourceFactory.get_resource(prep_dataset_identifier, temp_file)
+            self.storage_connector.set_resource(prep_dataset_identifier, streamed_resource)
+            streamed_resource = self.storage_connector.get_resource(prep_dataset_identifier)
+
         return streamed_resource
+
+    def prepare_dataset(self,
+                        h5py_file : h5py.File,
+                        raw_samples_identifier,
+                        raw_targets_identifier):
+
+        df = pd.read_csv('src/outlier_hub/datasets/ham10k/data/raw/train/train_labels/ISIC2018_Task3_Training_GroundTruth.csv', header=0)
+        
+        print(df.head())
+
+        df = df.sort_values(by = 'image')
+        labels_arr = df.iloc[:, 1:len(df.columns)].values
+
+        target_dset = h5py_file.create_dataset('targets', data = labels_arr)
+
+        sample_group = h5py_file.create_group('samples')
+
+        print(f'raw_samples_identifier): {raw_samples_identifier}')
+        split_samples = sorted(glob.glob(raw_samples_identifier + '/*.jpg'))
+        print(f'len(split_samples): {len(split_samples)}')
+
+        counter = 0
+        for img_path in split_samples:
+
+            # open image in binary, behind the path
+            with open(img_path, 'rb') as img:
+                img_binary = img.read()
+
+            img_binary_np = np.asarray(img_binary)
+
+            h5py_file = sample_group.create_dataset(str(counter), data=img_binary_np)
+            counter = counter + 1
